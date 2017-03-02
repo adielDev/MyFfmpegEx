7f915e29d7eee35f1cf798af07774fe2366950ed
adiel <adielbot@outlook.com>
2017-03-02 17:34:46 +0200

    reverse layout

--- app/src/main/java/adiel/myffmpegex/MainActivity.java
+++ app/src/main/java/adiel/myffmpegex/MainActivity.java
@@ -1,6 +1,8 @@
 package adiel.myffmpegex;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.Handler;
 import android.support.v4.os.ResultReceiver;
 import android.support.v7.app.AppCompatActivity;
@@ -10,22 +12,32 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
 import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static adiel.myffmpegex.Util.generateTimeStampVideoFileUri;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
 
+    private static final int IMAGE_PICKER_SELECT = 1;
     private  Handler uiHandler;
-
+    Uri selectedMediaUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
             findViewById(R.id.btnLoadBinari).setOnClickListener(this);
             findViewById(R.id.btnLoadBinariInSEperateProcess).setOnClickListener(this);
+            findViewById(R.id.btnPickVideo).setOnClickListener(this);
         uiHandler = new Handler();
 
     }
@@ -49,11 +61,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 intent.putExtra("receiver",myResultReciever);
                 LoadBinaryService.startActionLoadBinary(this,intent);
                 break;
+            case R.id.btnPickVideo:
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickIntent.setType("image/* video/*");
+                startActivityForResult(pickIntent, IMAGE_PICKER_SELECT);
+
+                break;
 
         }
     }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+        if (resultCode == RESULT_OK) {
+            selectedMediaUri = data.getData();
 
+            if (selectedMediaUri.toString().contains("images")) {
+                //handle image
+            } else if (selectedMediaUri.toString().contains("video")) {
+                //handle video
+                Uri vidPathOutput = generateTimeStampVideoFileUri("FfOutput");
+                String[] cmd= new String[]{"-i",selectedMediaUri.getPath(),"-b:v","2.4M","-bufsize","2.404M","-maxrate","5M","-preset","fast",vidPathOutput.getPath()};
+                exeCompress(cmd);
+                Toast.makeText(this, "start compress", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+    }
 
     private void loadBinary(){
         FFmpeg ffmpeg = FFmpeg.getInstance(getApplicationContext());
@@ -127,5 +161,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    public void compress(Uri videoUri) {
+        String vidPath = videoUri.getPath();
+        Uri vidPathOutput = generateTimeStampVideoFileUri("FfOutput");
+        String[] cmd= new String[]{"-i",vidPath,"-b:v","2.4M","-bufsize","2.404M","-maxrate","5M","-preset","fast",vidPathOutput.getPath()};
+        loadBinary();
+        exeCompress(cmd);
+
+    }
+
+
+    private void exeCompress(String[] cmd){
+        FFmpeg ffmpeg = FFmpeg.getInstance(getApplicationContext());
+        try {
+            // to execute "ffmpeg -version" command you just need to pass "-version"
+            ffmpeg.execute(cmd, new ExecuteBinaryResponseHandler() {
+
+                @Override
+                public void onStart() {
+                    Log.d("temp","exeCompress onStart");
+                    String s = "exeCompress onStart:";
+                    Log.d("temp",s);
+                }
+
+                @Override
+                public void onProgress(String message) {
+                    String s = "exeCompress onProgress:" + message;
+                    Log.d("temp",s);
+                }
+
+                @Override
+                public void onFailure(String message) {
+                    Toast.makeText(MainActivity.this, "onFailure", Toast.LENGTH_SHORT).show();
+                    String s = "exeCompress onFailure:" + message;
+                    Log.d("temp",s);
+                }
+
+                @Override
+                public void onSuccess(String message) {
+                    Toast.makeText(MainActivity.this, "onSuccess", Toast.LENGTH_SHORT).show();
+                    Log.d("temp","exeCompress onSuccess:"+message);
+                    String s = "exeCompress onSuccess:" + message;
+                    Log.d("temp",s);
+                }
+
+                @Override
+                public void onFinish() {
+                    Toast.makeText(MainActivity.this, "onFinish", Toast.LENGTH_SHORT).show();
+                    String s = "exeCompress onFinish:" ;
+                    Log.d("temp",s);
+                }
+            });
+        } catch (FFmpegCommandAlreadyRunningException e) {
+            // Handle if FFmpeg is already running
+            e.printStackTrace();
+            Log.d("temp","FFmpegCommandAlreadyRunningException");
+            String s = "FFmpegCommandAlreadyRunningException";
+            Log.d("temp",s);
+        }
+
+    }
 
 }
